9aed6ae93bb7db80b49d312762de461e71eaa12c
hamid <hamid@hamids-MacBook-Air.local>
2017-05-14 20:23:49 -0700

    first commit

---
+++ Multi-Steps/script.js
@@ -0,0 +1,60 @@
+/**
+ * Created by hamid on 5/14/17.
+ */
+
+// create our angular app and inject ngAnimate and ui-router
+// =============================================================================
+angular.module('formApp', ['ngAnimate', 'ui.router'])
+
+    // configuring our routes
+    // =============================================================================
+    .config(function($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+
+        // route to show our basic form (/form)
+            .state('form', {
+                url: '/form',
+                templateUrl: 'partial/form.html',
+                controller: 'formController'
+            })
+
+            // nested states
+            // each of these sections will have their own view
+            // url will be nested (/form/profile)
+            .state('form.profile', {
+                url: '/profile',
+                templateUrl: 'partial/form-profile.html'
+            })
+
+            // url will be /form/interests
+            .state('form.interests', {
+                url: '/interests',
+                templateUrl: 'partial/form-interests.html'
+            })
+
+            // url will be /form/payment
+            .state('form.payment', {
+                url: '/payment',
+                templateUrl: 'partial/form-payment.html'
+            });
+
+        // catch all route
+        // send users to the form page
+        $urlRouterProvider.otherwise('/form/profile');
+    })
+
+    // our controller for the form
+    // =============================================================================
+    .controller('formController', function($scope) {
+
+        // we will store all of our form data in this object
+        $scope.formData = {};
+
+        // function to process the form
+        $scope.processForm = function() {
+            alert('awesome!');
+        };
+
+    });
+
